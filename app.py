a5dd46a3e16d9d747aa64713ee73381ff6f84cd1
Wang <wong920907@gmail.com>
2024-03-28 12:19:57 +0800

    success deployed version

--- app.py
+++ app.py
@@ -35,14 +35,6 @@ def pretty_echo(event):
     
     if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
         
-        # # Phoebe 愛唱歌
-        # pretty_note = '♫♪♬'
-        # pretty_text = ''
-        
-        # for i in event.message.text:
-        
-        #     pretty_text += i
-        #     pretty_text += random.choice(pretty_note)
     
         line_bot_api.reply_message(
             event.reply_token,
