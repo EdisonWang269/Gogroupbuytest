82a7983153183a4eaee68e728e56b30f661397f7
Wang <wong920907@gmail.com>
2024-04-04 20:01:50 +0800

    git_update

--- app.py
+++ app.py
@@ -28,16 +28,6 @@ def git_update():
 def home():
     return render_template("index.html")
 
-@app.route('/update_server', methods=['POST'])
-def webhook():
-    if request.method == 'POST':
-        repo = git.Repo('path/to/git_repo')
-        origin = repo.remotes.origin
-        origin.pull()
-        return 'Updated PythonAnywhere successfully', 200
-    else:
-        return 'Wrong event type', 400
-
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
